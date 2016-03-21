34a067bdc97a7632e7b94a122939662840db82cd
Richard Shea <rshea@thecubagroup.com>
2016-03-21 17:02:15 +1300

    Settings changes to accomodate djangorestframework-jsonapi

--- roses/roses/settings.py
+++ roses/roses/settings.py
@@ -128,9 +128,25 @@ STATIC_URL = '/static/'
 
 
 
+#Everything except DEFAULT_PERMISSION_CLASSES and PAGE_SIZE was added
+#to REST_FRAMEWORK on 20160321 to accomodate djangorestframework-jsonapi
+#(https://github.com/django-json-api/django-rest-framework-json-api)
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework_json_api.pagination.PageNumberPagination',
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework_json_api.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser'
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework_json_api.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_METADATA_CLASS': 'rest_framework_json_api.metadata.JSONAPIMetadata',
 }
 
 #Configuration of django-cors-header
